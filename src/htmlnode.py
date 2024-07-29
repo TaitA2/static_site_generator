55033e36ca6db13c5afbac5b324e6b5b95a832a1
TaitA2 <adam.tait617@gmail.com>
2024-07-29 13:28:07 +0100

    add LeafNode child class of HTMLNode in htmlnode.py

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -12,15 +12,44 @@ class HTMLNode():
     def props_to_html(self):
         # returns prop dict formatted as KEY="VALUE" eg "target": "_blank" becomes target="_blank"
         d = self.props
-        d_list = [f' {key}="{value}"' for key, value in d.items()]
-        return "".join(d_list)
+        if d:
+            d_list = [f' {key}="{value}"' for key, value in d.items()]
+            return "".join(d_list)
+        return ""
 
     def __repr__(self):
         # return string representation of HTMLNode object
         return f"HTMLNode(tag={self.tag}, value={self.value}, children={self.children}, props={self.props})"
 
+
+# LeafNode child class of HTMLNode 
+class LeafNode(HTMLNode):
+
+    # similar to parent but no children and value required
+    def __init__(self, tag=None, value=None, props=None):
+        super().__init__(tag, value, None, props)
+        
+
+    def to_html(self):
+        
+        # raise ValueError if missing value
+        if self.value == None:
+            raise ValueError
+        
+        # return value as raw text if no tag
+        if not self.tag:
+            return self.value
+
+        # convert to html
+        props_string = self.props_to_html()
+        return f"<{self.tag}{props_string}>{self.value}</{self.tag}>"
+
+
+
 def main():
-    test = HTMLNode("h1", "header", [], {"href": "https://google.com"})
-    print(test.props_to_html())
+    test =  LeafNode("p", "This is a paragraph of text.")
+    test2 = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+    print(test.to_html())
+    print(test2.to_html())
 
 main()
