88e9291bee0f3745bc09337032429499d16a26ed
TaitA2 <adam.tait617@gmail.com>
2024-07-30 11:39:53 +0100

    add split_nodes_delimiter func to textnode.py

--- src/textnode.py
+++ src/textnode.py
@@ -13,9 +13,28 @@ class TextNode():
         # return string representation of TextNode object
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
 
+# convert one text node into list of text nodes based on delimiter e.g ' = code or ** = bold 
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != "text":
+            new_nodes.append(old_node)
+        else:
+            split_node = old_node.text.split(delimiter)
+            for i in range(len(split_node)):
+                if i != 1:
+                    new_nodes.append(TextNode(split_node[i], "text"))
+                else:
+                    new_nodes.append(TextNode(split_node[i], text_type))
+    return new_nodes
+
 def main():
-    test = TextNode("this is a test", "link", "https://www.google.com" )
-    print(test)
+    # test = TextNode("this is a test", "link", "https://www.google.com" )
+    # print(test)
+    node = TextNode("This is text with a **bold** word", "text")
+    new_nodes = split_nodes_delimiter([node], "**", "bold")
+    for n in new_nodes:
+        print(n)
 
 if __name__ == "__main__":
     main()
