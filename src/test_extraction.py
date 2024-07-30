2e126c2c83b4ddec4f7611ca67d315c2247a7bd8
TaitA2 <adam.tait617@gmail.com>
2024-07-30 12:40:15 +0100

    add unit tests for regex extraction

--- src/test_extraction.py
+++ src/test_extraction.py
@@ -1,1 +1,23 @@
 import unittest
+
+from extraction import *
+
+class TestImageExtraction(unittest.TestCase):
+    def test_img(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        result = extract_markdown_images(text)
+        expected = [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")]
+        print(f"\nExpected:\n{expected}\nActual:\n{result}")
+        self.assertEqual(result, expected)
+
+
+class TestLinkExtraction(unittest.TestCase):
+    def test_link(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        result = extract_markdown_links(text)
+        expected = [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")]
+        print(f"\nExpected:\n{expected}\nActual:\n{result}")
+        self.assertEqual(result, expected)
+
+if __name__ == "__main__":
+    unittest.main()
