a84758bc1adccda1121c37ea2e3254e9f3167d54
TaitA2 <adam.tait617@gmail.com>
2024-07-30 16:59:32 +0100

    add markdown_to_block() func in split_blocks.py

--- TODO.md
+++ TODO.md
@@ -1,3 +1,4 @@
 # TODO
 
-* Fix bug in node conversion
+* fix split_blocks.py markdown_to_block() function. 
+    * does not strip whitespace or remove empty blocks
--- src/split_blocks.py
+++ src/split_blocks.py
@@ -1,11 +1,28 @@
 # convert raw markdown to list of "block" strings
 
 def markdown_to_block(markdown):
-    blocks = markdown.split("\n")
+    blocks = markdown.split("\n\n")
+    for block in blocks:
+        block.strip()
+        if block == "":
+            blocks.remove(block)
+    return blocks
+
 
 
 
 def main():
-    pass
+    markdown = """# This is a heading
+
+This is a paragraph of text. It has some **bold** and *italic* words inside of it.
+
+* This is the first list item in a list block
+* This is a list item
+* This is another list item"""
+    blocks = markdown_to_block(markdown)
+    for b in blocks:
+        print(f"\nblock:\n{b}")
+
 if __name__ == "__main__":
     main()
+
