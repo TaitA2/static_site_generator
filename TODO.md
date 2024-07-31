2bb8433b2f6c378e3fdc98d0aab361a0355ed6de
TaitA2 <adam.tait617@gmail.com>
2024-07-31 10:27:41 +0100

    fix markdown_to_block()

--- TODO.md
+++ TODO.md
@@ -1,4 +1,3 @@
 # TODO
 
-* fix split_blocks.py markdown_to_block() function. 
-    * does not strip whitespace or remove empty blocks
+*
--- src/split_blocks.py
+++ src/split_blocks.py
@@ -2,11 +2,12 @@
 
 def markdown_to_block(markdown):
     blocks = markdown.split("\n\n")
+    cleaned_blocks = []
     for block in blocks:
-        block.strip()
-        if block == "":
-            blocks.remove(block)
-    return blocks
+        block = block.strip()
+        if block != "":
+            cleaned_blocks.append(block)  
+    return cleaned_blocks
 
 
 
