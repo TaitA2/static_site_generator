7169954dc0cdd8a148a9dc3b0a538787f0fbc815
TaitA2 <adam.tait617@gmail.com>
2024-08-01 14:22:45 +0100

    imporve italics in lists and add links / imgs in lists

--- src/node_conversion.py
+++ src/node_conversion.py
@@ -7,6 +7,18 @@ import re
 tag_dict = {"bold": "b", "italic": "i", "code": "code", "link": "a", "img": "img", "list_item": "li",   # inline tags
             "paragraph": "p", "heading": "h", "code": "code", "quote": "blockquote", "unordered_list": "ul", "ordered_list": "ol" }  # block tags
 
+# convert raw string to list of TextNodes
+def text_to_textnodes(text):
+    text_node = TextNode(text, "text",)
+    img_split = split_nodes_image([text_node])
+    link_split = split_nodes_link(img_split)
+    code_split = split_nodes_delimiter(link_split, "`", "code")
+    bold_split = split_nodes_delimiter(code_split, "**", "bold")
+    italic_split = split_nodes_delimiter(bold_split, "*", "italic")
+    alt_italic_split = split_nodes_delimiter(italic_split, "_", "italic")
+    
+    return alt_italic_split
+
 # convert text node to html node
 def text_node_to_html_node(text_node):
     text = text_node.text
@@ -33,17 +45,6 @@ def text_node_to_html_node(text_node):
     else:
         print(text_type)
         raise Exception("Invalid text type")
-
-# convert raw string to list of TextNodes
-def text_to_textnodes(text):
-    text_node = TextNode(text, "text",)
-    img_split = split_nodes_image([text_node])
-    link_split = split_nodes_link(img_split)
-    code_split = split_nodes_delimiter(link_split, "`", "code")
-    bold_split = split_nodes_delimiter(code_split, "**", "bold")
-    italic_split = split_nodes_delimiter(bold_split, "*", "italic")
-    
-    return italic_split
 
 
 def markdown_to_html_node(markdown):
@@ -75,19 +76,21 @@ def markdown_to_html_node(markdown):
             list_items = re.split(r"\n *\* +", block[1:])
             list_items = [item for item in list_items if item]
             # add italics to list items
-            list_items = [li.replace(" *", " <i>").replace("* ", "</i> ").replace(" _", "  1<i>").replace("_ ", "</i>  1") for li in list_items]
-            children = [LeafNode("li", li.strip()) for li in list_items]
+            text_nodes = [(text_to_textnodes(f"<li>{li}</li>")) for li in list_items]
+            text_nodes = [item for sublist in text_nodes for item in sublist]
 
         elif block_type == ("ordered_list"):
             list_items = re.split(r"\d+\. ", block)
             list_items = [item for item in list_items if item]
             # add italics to list items
-            list_items = [li.replace(" *", " <i>").replace("* ", "</i> ").replace(" _", "  1<i>").replace("_ ", "</i>  1") for li in list_items]
-            children = [LeafNode("li", li.strip()) for li in list_items]
+            text_nodes = [(text_to_textnodes(f"<li>{li}</li>")) for li in list_items]
+            text_nodes = [item for sublist in text_nodes for item in sublist]
 
         else:
             text_nodes = text_to_textnodes(block)
-            children = [text_node_to_html_node(node) for node in text_nodes]
+        children = [text_node_to_html_node(node) for node in text_nodes]
+        # print(text_nodes)
+        
 
         
         
@@ -109,13 +112,14 @@ test_node = TextNode("bold test", "bold")
 markdown = """
 # This is the heading.
 
-This is a **bold** word, an *italic* word, and a `code word` in a paragraph
+This is text with a **bold** word, an *italic* word, a `code block`, an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg), and a [link](https://boot.dev)
 
  * This is an unordered list block
  * with three list items
  * like this one
  * AND THIS RISKY *ITALIC* ONE
  * AND THE ALTERNATIVE _ITALIC_ ONE
+ * but does it support a [link](https://boot.dev)
 
  1. This is an ORDERED list
  2. I don't think it works
@@ -124,4 +128,4 @@ This is a **bold** word, an *italic* word, and a `code word` in a paragraph
 """
 html_node = markdown_to_html_node(markdown)
 print(html_node)
-print(">\n<".join(html_node.split("><")))
+# print(">\n<".join(html_node.split("><")))
