8e8aa497ded0141187ae030ba6508605ee5a0e97
TaitA2 <adam.tait617@gmail.com>
2024-08-01 16:05:41 +0100

    add recursive copy func to main.py

---
+++ src/main.py
@@ -0,0 +1,26 @@
+import os
+import shutil
+
+# recursively copy contents of source dir to destination dir
+def recursive_copy(source, dest):
+    # make destination dir if none exists
+    if not os.path.exists(dest):
+        os.mkdir(dest)
+    # iterate over contents of source dir
+    for file in os.listdir(source):
+        # set paths to be source/file and dest/file
+        from_path = os.path.join(source, file)
+        dest_path = os.path.join(dest, file)
+        # print paths for insight and debugging
+        print(f"* {from_path} -> {dest_path}")
+        # copy files across and call this func on dirs
+        if os.path.isfile(from_path):
+            shutil.copy(from_path, dest_path)
+        else:
+            recursive_copy(from_path, dest_path)
+
+def main():
+    # call recursive copy func from static/ to public/
+    recursive_copy("static/", "public/")
+
+main()
